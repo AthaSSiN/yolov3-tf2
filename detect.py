0797eda4637f9d847fbe5ef6794aa97e34702239
Atharv Singh Patlan <56124496+AthaSSiN@users.noreply.github.com>
2020-03-30 12:09:45 +0530

    Updated detect.py
    Implemented Mean IOU calculation and added functionality to run the model on multiple images in the TFRecords for detection

--- detect.py
+++ detect.py
@@ -9,6 +9,8 @@ from yolov3_tf2.models import (
 )
 from yolov3_tf2.dataset import transform_images, load_tfrecord_dataset
 from yolov3_tf2.utils import draw_outputs
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 
 flags.DEFINE_string('classes', './data/coco.names', 'path to classes file')
 flags.DEFINE_string('weights', './checkpoints/yolov3.tf',
@@ -20,6 +22,30 @@ flags.DEFINE_string('tfrecord', None, 'tfrecord instead of image')
 flags.DEFINE_string('output', './output.jpg', 'path to output image')
 flags.DEFINE_integer('num_classes', 80, 'number of classes in the model')
 
+def iou(box1, box2):
+    """Implement the intersection over union (IoU) between box1 and box2
+    Arguments:
+    box1 -- first box, list object with coordinates (x1, y1, x2, y2)
+    box2 -- second box, list object with coordinates (x1, y1, x2, y2)
+    """
+
+    # Calculate the (y1, x1, y2, x2) coordinates of the intersection of box1 and box2. Calculate its Area.
+    xi1 = np.maximum(box1[0], box2[0])
+    yi1 = np.maximum(box1[1], box2[1])
+    xi2 = np.minimum(box1[2], box2[2])
+    yi2 = np.minimum(box1[3], box2[3])
+    # Case in which they don't intersect --> max(,0)
+    inter_area = max(xi2-xi1, 0)*max(yi2-yi1, 0)
+
+    # Calculate the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
+    box1_area = (box1[2]-box1[0])*(box1[3]-box1[1])
+    box2_area = (box2[2]-box2[0])*(box2[3]-box2[1])
+    union_area = box1_area + box2_area - inter_area
+
+    # compute the IoU
+    iou = float(inter_area)/float(union_area)
+
+    return iou
 
 def main(_argv):
     physical_devices = tf.config.experimental.list_physical_devices('GPU')
@@ -38,32 +64,60 @@ def main(_argv):
     logging.info('classes loaded')
 
     if FLAGS.tfrecord:
+        n = 70
         dataset = load_tfrecord_dataset(
             FLAGS.tfrecord, FLAGS.classes, FLAGS.size)
         dataset = dataset.shuffle(512)
-        img_raw, _label = next(iter(dataset.take(1)))
+        set = iter(dataset.take(n))
+        for i in range(n):
+            img_raw, _label = next(set)
+            img = tf.expand_dims(img_raw, 0)
+            img = transform_images(img, FLAGS.size)
+        
+            t1 = time.time()
+            boxes, scores, classes, nums = yolo(img)
+            t2 = time.time()
+           
+            meanIou = 0
+            classCount = 4
+            for k in range(nums[0]):
+                arr_iou=[0]
+                for j in range(classCount):
+                    arr_iou.append(iou(boxes[0][k],_label[j]))
+                    if(_label[j,4] == 0):
+                       classCount = j
+                       break
+                meanIou += max(arr_iou)
+                
+            print(f" time: {t2-t1},   Mean IOU : {meanIou/max(classCount, 1)},   detected classes : {nums[0]},   actual classes : {classCount}")    
+            img = cv2.cvtColor(img_raw.numpy(), cv2.COLOR_RGB2BGR)
+            img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
+           
+            cv2.imwrite(f'./output{i}.jpg', img)
+            
+            
     else:
         img_raw = tf.image.decode_image(
             open(FLAGS.image, 'rb').read(), channels=3)
 
-    img = tf.expand_dims(img_raw, 0)
-    img = transform_images(img, FLAGS.size)
-
-    t1 = time.time()
-    boxes, scores, classes, nums = yolo(img)
-    t2 = time.time()
-    logging.info('time: {}'.format(t2 - t1))
-
-    logging.info('detections:')
-    for i in range(nums[0]):
-        logging.info('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
-                                           np.array(scores[0][i]),
-                                           np.array(boxes[0][i])))
-
-    img = cv2.cvtColor(img_raw.numpy(), cv2.COLOR_RGB2BGR)
-    img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
-    cv2.imwrite(FLAGS.output, img)
-    logging.info('output saved to: {}'.format(FLAGS.output))
+        img = tf.expand_dims(img_raw, 0)
+        img = transform_images(img, FLAGS.size)
+    
+        t1 = time.time()
+        boxes, scores, classes, nums = yolo(img)
+        t2 = time.time()
+        logging.info('time: {}'.format(t2 - t1))
+    
+        logging.info('detections:')
+        for i in range(nums[0]):
+            logging.info('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
+                                               np.array(scores[0][i]),
+                                               np.array(boxes[0][i])))
+    
+        img = cv2.cvtColor(img_raw.numpy(), cv2.COLOR_RGB2BGR)
+        img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
+        cv2.imwrite(FLAGS.output, img)
+        logging.info('output saved to: {}'.format(FLAGS.output))
 
 
 if __name__ == '__main__':
